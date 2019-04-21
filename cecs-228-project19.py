bf5e3ecd7baed8ea33b69ed5fe3bddfe35089291
Jessica Wei <jessicawei9@gmail.com>
2019-04-21 04:40:03 -0700

    verified that using Dictionary does not give us any kind of shortcut in terms of ordering the IDs

--- cecs-228-project19.py
+++ cecs-228-project19.py
@@ -1,1 +1,6 @@
+#CECS 228 Project Code
 #Contributors: Linda Trinh, Hanson Nguyen, Bryan Vu, Jessica Wei
+
+Dict = {123: "Linda Trinh", 321: "Hanson Nguyen", 213: "Bryan Vu", 231: "Jessica Wei"}
+print(Dict) #This ends up okay, it's not cheating. The Dictionary itself does not print itself in order based on given keys.
+
