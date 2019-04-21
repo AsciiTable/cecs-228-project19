bcaf6a675618cf8cd87628916273df75c97796a7
Jessica Wei <jessicawei9@gmail.com>
2019-04-21 04:40:38 -0700

    updated comment, I'm going to sleep now

--- cecs-228-project19.py
+++ cecs-228-project19.py
@@ -2,5 +2,5 @@
 #Contributors: Linda Trinh, Hanson Nguyen, Bryan Vu, Jessica Wei
 
 Dict = {123: "Linda Trinh", 321: "Hanson Nguyen", 213: "Bryan Vu", 231: "Jessica Wei"}
-print(Dict) #This ends up okay, it's not cheating. The Dictionary itself does not print itself in order based on given keys.
+print(Dict) #The Dictionary itself does not print itself in order based on given keys. AKA it's not cheating.
 
