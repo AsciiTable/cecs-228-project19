3bca0c057e77cfdb03a1d1e19542f3c89fcad56a
Jessica Wei <jessicawei9@gmail.com>
2019-04-22 00:07:37 -0700

    program finished -- will do formatting later

--- cecs-228-project19.py
+++ cecs-228-project19.py
@@ -1,6 +1,40 @@
 #CECS 228 Project Code
 #Contributors: Linda Trinh, Hanson Nguyen, Bryan Vu, Jessica Wei
 
-Dict = {123: "Linda Trinh", 321: "Hanson Nguyen", 213: "Bryan Vu", 231: "Jessica Wei"}
-print(Dict) #The Dictionary itself does not print itself in order based on given keys. AKA it's not cheating.
+def part(idArr, low, high):
+    i = (low - 1)
+    piv = idArr[high]
 
+    for z in range(low, high):
+        if idArr[z] <= piv:
+            i = i + 1
+            idArr[i],idArr[z] = idArr[z],idArr[i]
+
+    idArr[i + 1],idArr[high] = idArr[high],idArr[i + 1]
+    return(i+1)
+
+def quickSort(idArr, low, high):
+    if low < high:
+        p1 = part(idArr, low, high)
+        quickSort(idArr, low, p1 - 1)
+        quickSort(idArr, p1 + 1, high)
+
+
+studentDict = {321: "Linda Trinh", 123: "Hanson Nguyen", 231: "Bryan Vu", 213: "Jessica Wei"}
+print("Unsorted Students by ID: ", studentDict) 
+idArray = []
+for x in studentDict:
+    idArray.append(x)
+
+length = len(idArray)
+quickSort(idArray, 0, (length - 1))
+
+last = idArray[length - 1]
+
+sortedList = "Sorted Students by ID:";
+for x in idArray:
+    sortedList = sortedList + " " + str(x) + ": "+ studentDict[x]
+    if x != last:
+        sortedList = sortedList + ","
+
+print(sortedList)
